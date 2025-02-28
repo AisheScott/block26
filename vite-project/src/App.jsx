51416c8a3feb63a54607f26f5787f38ff023b21b
AISHE Scott <scott.aishe@hexagon.com>
2025-02-27 19:03:52 -0500

    update

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
+import "./App.css";
+
+function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>{selectedContactId ? 
+    <div> <SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId} /> </div> 
+    : <ContactList setSelectedContactId={setSelectedContactId}/>}</>
+  );
+}
+
+export default App;
